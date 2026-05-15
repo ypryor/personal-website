a1e630fe46e044b7ca6db26d87ba980b1e2305a7
Yemko Pryor <pryoryemko@gmail.com>
2026-05-14 21:34:46 -0400

    updating personal webiste info with Life Research

--- src/data/affiliations.ts
+++ src/data/affiliations.ts
@@ -6,33 +6,15 @@ export interface Affiliation {
 
 export const affiliations: Affiliation[] = [
   {
-    name: 'Michigan Institute for Data & AI in Society',
-    sub: 'MIDAS',
-    href: 'https://midas.umich.edu/directory/tina-lasisi/',
+    name: 'Lasisi Lab',
+    sub: 'Department of Anthropology · University of Michigan',
+    href: 'https://www.lasisilab.com/',
   },
-  {
-    name: 'Genome Science Training Program',
-    sub: 'Center for Statistical Genetics',
-    href: 'https://sph.umich.edu/csg/gstp_pages/gstpfaculty.html',
-  },
-  {
-    name: 'Center for Computational Medicine & Bioinformatics',
-    sub: 'CCMB',
-    href: 'https://medschool.umich.edu/departments/computational-medicine-bioinformatics/ccmb',
-  },
-  {
-    name: 'African Studies Center',
-    sub: 'International Institute',
-    href: 'https://ii.umich.edu/ii/people/all/t/tlasisi.html',
-  },
-  {
-    name: 'Center for Global Health Equity',
-    sub: 'CGHE',
-    href: 'https://globalhealthequity.umich.edu/membership/members/tlasisi',
-  },
-  {
-    name: 'Center for Ethics, Society, & Computing',
-    sub: 'ESC',
-    href: 'https://esc.umich.edu/faculty/',
-  },
+  // TODO: Add any other center/program affiliations you hold at UMich below.
+  // Example format:
+  // {
+  //   name: 'Michigan Institute for Data & AI in Society',
+  //   sub: 'MIDAS',
+  //   href: 'https://midas.umich.edu/',
+  // },
 ];
